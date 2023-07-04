25bd3fe295fa3bcc027d9f1f7f35674113b5a917
snehalbabar <h728e344@wichita.edu>
2023-07-04 17:57:43 -0500

    Adding services layer to add cities name

---
+++ ServiceLayer/CitiesService.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceLayer;
+public class CitiesService
+{
+    private List<string> _cities;
+
+    public CitiesService()
+    {
+        _cities = new List<string>()
+        {
+            "London",
+            "paris",
+            "Delhi",
+            "Pune",
+            "Dallas"
+        };
+    }
+
+    public List<string> GetCities()
+    {
+        return _cities;
+
+    }
+
+}
+
